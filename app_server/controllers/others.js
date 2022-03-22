e5db15a4e7570134e022cf831cc48b22271de37c
gra-m <91182298+gra-m@users.noreply.github.com>
2022-03-22 13:06:20 +0100

    closes#7 multiline was nothing to do with pug as interpolated string, copied replace from existing, added tab replace chained too

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,18 +1,12 @@
 /*GET about*/
-const about = (req, res) => res.render('about', {
-    title: 'Who are we?',
-    pageHeader: {
-        title: 'About Loc8r',
-        strapline: '',
-    },
-    otherContent: {
-        extra1: 'Loc8r was created as part of my learning the MEAN stack, which I\'ve got to say has been pretty enjoyable so far.' +
-            ' Pug is interesting, very succinct, took some getting used to. ' +
-            'Express works, and the book I\'m following provided a good MVC template.',
-        extra2: '',
+const about = (req, res) => res.render('about',
+    {
+        title: 'Who are we?',
+        content: 'Loc8r was created as part of my learning the MEAN stack, which I\'ve got to say has been pretty enjoyable so far.\n\n' +
+            '\tPug is interesting, very succinct, but took some getting used to.\n' +
+            '\tExpress works, and the book I followed (see below) provided a good MVC template.',
     }
-
-});
+);
 
 module.exports = {
     about
